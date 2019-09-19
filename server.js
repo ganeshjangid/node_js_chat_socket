77f055dae5eef0efa06c80cc6c72090680439c14
ganeshjangid <webdeveloper.ganesh@gmail.com>
2019-09-19 13:55:55 +0530

    test

--- server.js
+++ server.js
@@ -31,5 +31,30 @@ const server=app.listen(port,()=> console.log(`This server is running on ${port}
 const io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
-    console.log('a user connected');
+    console.log('user connected');
+
+    socket.username="Ganesh suthar";
+
+    //listen on change username
+
+    socket.on('change_username', (data) => {
+        socket.username=data.username;
+        //console.log(data.username);
+    });
+
+     //listen on new_message
+     socket.on('new_message', (data) => {
+         //broadcast the new message
+         io.sockets.emit('new_message', {
+             message: data.message,
+             username: socket.username
+         });
+     });
+
+     //listen on typing
+     socket.on('typing', (data) => {
+         socket.broadcast.emit('typing', {
+             username: socket.username
+         })
+     });
 });
